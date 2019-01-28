baf3c800f4a6fa9b46814fa6e42d250eb49d58c7
vvasc <vinedias@gmail.com>
2019-01-28 21:31:44 -0200

    chore(appcontroller): Removing AppController

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Connection } from 'typeorm';
+
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { BikeModule } from './bike/bike.module';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(),
     UserModule,
+    AuthModule,
+    BikeModule,
   ],
-  controllers: [AppController],
   providers: [
     AppService,
   ],
